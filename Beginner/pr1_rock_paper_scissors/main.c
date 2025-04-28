81f1f78199eb65ca43a43fd9a5e4a6274adbcc71
Beigi <m.mohammadbeigi28@gmail.com>
2025-04-28 19:54:39 +0330

    Merge branch 'main' of https://github.com/Mohammad-Beigi/C_Project

--- Beginner/pr1_rock_paper_scissors/main.c
+++ Beginner/pr1_rock_paper_scissors/main.c
@@ -2,17 +2,80 @@
 
 int main()
 {
-    int player1 = 0;
-    printf("...<<< Rock Paper Scissor >>>...");
+    char player1 = 0;
+    char player2 = 0;
+
+    printf("...<<< Rock Paper Scissor >>>...\n");
+
+    srand(time(NULL));
 
     while(TRUE)
     {
-        printf("Enter your choise:\n");
-        printf("Rock------> 1\n");
-        printf("Paper-----> 2\n");
-        printf("Scissor---> 3\n");
-        scanf("%d", &player1);
-        printf("You choose %d\n", player1);
+        printf("Enter 'r' for 'Rock', 'p' for Paper, and 's' for 'Scissor': ");
+        scanf("%c", &player1);
+        
+        switch (player1)
+        {
+        case 'r':
+            printf("You entered 'Rock'\n");
+            break;
+        case 'p':
+            printf("You entered 'Paper'\n");
+            break;
+        case 's':
+            printf("You entered 'Scissor'\n");
+            break;
+        default:
+            printf("You entered a wrong character. Try again.\n");
+            break;
+        }
+
+        switch (rand()%3)
+        {
+        case 0:
+            player2 = 'r';
+            break;
+        case 1:
+            player2 = 'p';
+            break;
+        case 2:
+            player2 = 's';
+            break;
+        default:
+            break;
+        }
+
+        switch (game(player1, player2))
+        {
+        case 0:
+            printf("You lose!\n");
+            break;
+        case 1:
+            printf("Draw\n");
+            break;
+        case 2:
+            printf("You Win!\n");
+            break;
+        default:
+            break;
+        }
+        getchar();
     }
     return 0;
+}
+
+int game(char const p1, char const p2)
+{
+    if (p1 == p2)
+        return 1;
+    else if (p1=='s' && p2=='p')
+        return 2;
+    else if (p1=='p' && p2=='s')
+        return 0;
+    else if (p1=='r' && p2=='p')
+        return 0;
+    else if (p1=='p' && p2=='r')
+        return 2;
+    else
+        return -1;
 }
--- Beginner/pr1_rock_paper_scissors/main.h
+++ Beginner/pr1_rock_paper_scissors/main.h
@@ -1,4 +1,14 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define TRUE  1
 #define FALSE 0
+
+int game(char const p1, char const p2);
+
+
+#endif
